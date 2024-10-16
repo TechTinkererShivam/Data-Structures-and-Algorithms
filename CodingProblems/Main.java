7238894f93ca59eb981fbc0ebd1f21736392c263
TechTinkererShivam <shivamtech.iithyd27@gmail.com>
2024-10-16 12:51:23 +0530

    minor changes

--- CodingProblems/Main.java
+++ CodingProblems/Main.java
@@ -4,21 +4,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-
-
-
-
-
-
-
-
-//        int[] arr = {10,20,45,80,65,15};
+//                int[] arr = {10,20,45,80,65,15};
 //        for(int ele : arr){
 //            System.out.print(ele+" ");
 //        }
 //        System.out.println();
 //
 //        arr = null; // This means the reference (arr) doesn't point to any array object in memory.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- LinkedList/Implementation.java
+++ LinkedList/Implementation.java
@@ -1,5 +1,17 @@
 package LinkedList;
 
+class SinglyLinkedList{ // user defined data structure
+    Node head;
+    Node tail;
+    int size;
+
+    void addAtEnd(int val){
+
+    }
+
+}
+
+
 public class Implementation {
     public static void main(String[] args) {
 
